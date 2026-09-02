f3f1ac77aa1b419560a08be65cd889a7f3b10df1
Nathan <nathannxx@proton.me>
2026-09-02 08:21:46 +0700

    Apply saved language at application startup

--- pio/src/main/java/com/mio/kitchen/PIO.kt
+++ pio/src/main/java/com/mio/kitchen/PIO.kt
@@ -1,5 +1,10 @@
 package com.mio.kitchen
 
 import android.app.Application
+import android.content.Context
 
-class PIO : Application()
+class PIO : Application() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(LanguageManager.apply(base))
+    }
+}
